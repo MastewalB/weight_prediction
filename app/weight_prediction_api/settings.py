1661fa36f0b702a368fcfbf4f875a4cd6d700f5c
Mastewal B <se.mastewal.belete@gmail.com>
2021-10-26 10:04:27 +0300

    Rearrangements

--- app/weight_prediction_api/settings.py
+++ app/weight_prediction_api/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MODELS = os.path.join(BASE_DIR, 'ml/models')
@@ -21,7 +24,7 @@ MODELS = os.path.join(BASE_DIR, 'ml/models')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oh^ju*n_zb-t1pbd68ilqe9lm)c1k$k@gtq1jcjepfakrnu5w3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
